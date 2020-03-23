284973f07450e9a76ead8384bd8a647b73f469a7
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-03-23 03:16:14 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://demo:demo@cluster0-niks7.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'Cluster0'
 };
+
